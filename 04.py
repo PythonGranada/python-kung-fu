dd3e1277770f8ed597f4dc6788484b3087ff686e
Víctor Terrón <vterron@iaa.es>
2015-06-11 16:25:22 +0200

    Add challenge 04 (number of digits)

---
+++ 04.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+
+from __future__ import print_function
+
+# Author: Victor Terron (c) 2015
+# Email: `echo vt2rron1iaa32s | tr 132 @.e`
+# License: GNU GPLv3
+
+""" How do you count the number of digits of an integer? """
+
+def ndigits(number):
+    pass # place magic here
+
+if __name__ == "__main__":
+
+    print(ndigits(3))   # 1
+    print(ndigits(-5))  # 1
+    print(ndigits(27))  # 2
+    print(ndigits(495)) # 3
+
