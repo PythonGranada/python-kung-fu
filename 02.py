0a770dea6137a3d1849ddebe90e89a1b38f8dbaf
Víctor Terrón <vterron@iaa.es>
2015-06-11 16:06:22 +0200

    Add challenge 02 (product of integers)

---
+++ 02.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+from __future__ import print_function
+
+# Author: Victor Terron (c) 2015
+# Email: `echo vt2rron1iaa32s | tr 132 @.e`
+# License: GNU GPLv3
+
+""" Write a function that, given a list of integers, returns the product of
+all the elements. You are not allowed to use neither a for nor a while loop. """
+
+def product(numbers):
+    pass # place magic here
+
+if __name__ == "__main__":
+
+    numbers = [2, 3, 5]
+    print(product(numbers)) # 30
