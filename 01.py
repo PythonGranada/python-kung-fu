09c4fb1f32e8077bcbf3d6a3a996eabe34a283e9
Víctor Terrón <vterron@iaa.es>
2015-06-11 15:56:46 +0200

    Add challenge 01 (unique characters)

---
+++ 01.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+
+from __future__ import print_function
+
+# Author: Victor Terron (c) 2015
+# Email: `echo vt2rron1iaa32s | tr 132 @.e`
+# License: GNU GPLv3
+
+""" Write a function that returns whether a string has all unique characters.
+For extra points, do it without using any additional data structure (such as
+lists or dictionaries). You can use any function in the standard library. """
+
+def all_unique(word):
+    pass # place magic here
+
+if __name__ == "__main__":
+
+    print(all_unique("abcde")) # True
+    print(all_unique("abcda")) # False (two a's)
+
