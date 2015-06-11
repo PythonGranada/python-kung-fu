5a7bf2c06dd60475eb3db9962b647951b23ad9d0
Víctor Terrón <vterron@iaa.es>
2015-06-11 15:46:39 +0200

    Add challenge 00 (string permutations)

---
+++ 00.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+from __future__ import print_function
+
+# Author: Victor Terron (c) 2015
+# Email: `echo vt2rron1iaa32s | tr 132 @.e`
+# License: GNU GPLv3
+
+""" How do you determine whether a string is a permutation of another? """
+
+def is_permutation(word, another):
+    pass # place magic here
+
+if __name__ == "__main__":
+
+    word = "abbc"
+    print(is_permutation(word, "acaa")) # False
+    print(is_permutation(word, "babc")) # True
+
