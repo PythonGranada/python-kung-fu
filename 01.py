22116ad1ef5e9d2ab53df4831e7702babe7e4211
vterron <vterron@localhost>
2015-09-18 22:25:26 +0100

    Add unit tests for challenge 01

--- 01.py
+++ 01.py
@@ -10,11 +10,23 @@ from __future__ import print_function
 For extra points, do it without using any additional data structure (such as
 lists or dictionaries). You can use any function in the standard library. """
 
+import unittest
+
 def all_unique(word):
     pass # place magic here
 
+class AllUniqueTests(unittest.TestCase):
+
+  def test_all_unique(self):
+
+    self.assertTrue (all_unique(""))
+    self.assertTrue (all_unique("a"))
+    self.assertTrue (all_unique("B"))
+    self.assertTrue (all_unique("abcde"))
+    self.assertTrue (all_unique("aBcDe"))
+    self.assertFalse(all_unique("aa"))    # Two a's
+    self.assertFalse(all_unique("bB"))    # Two b's (in different cases)
+    self.assertFalse(all_unique("abCdc")) # Two c's
+
 if __name__ == "__main__":
-
-    print(all_unique("abcde")) # True
-    print(all_unique("abcda")) # False (two a's)
-
+    unittest.main()
