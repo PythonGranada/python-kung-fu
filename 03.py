350efbe5d1308faf9bfc196a57f64a8daff5f5a4
pablogsal <pablogsal@gmail.com>
2015-11-27 17:06:27 +0100

    Added unit tests to 03.py and some cool explanations. Also added love.

--- 03.py
+++ 03.py
@@ -10,11 +10,32 @@ from __future__ import print_function
 You can make at most one call to the 'in' operator or to the methods index()
 and find() """
 
+import unittest
+
 def is_rotation(word, another):
-    pass # place magic here
+    return pass # place magic here
+
+
+class RotationTests(unittest.TestCase):
+    def test_rotation(self):
+        self.assertTrue(is_rotation("Hari Seldon","ldonHari Se"))
+        self.assertTrue(is_rotation("abcdefg", "abcdefg"))
+        self.assertTrue(is_rotation("abcdefg", "cdefgab"))
+        self.assertTrue(is_rotation("ABCdefg", "CdefgAB"))
+        self.assertTrue(is_rotation("A", "A"))
+        # The following one is a little tricky. It depends on your definition of "permutation". In the
+        # "active" definition, as there are biyections between {} and itself ( see [1] ) then the
+        # assertion is True. In the "passive" definition it could be false ( see [2] ).
+        # [1] http://math.stackexchange.com/questions/140879/does-there-exist-a-bijection-between-empty-sets
+        # [2] Operations on the Empty set for discussion: https://en.wikipedia.org/wiki/Empty_set
+        self.assertTrue(is_rotation("", "")) # Because reasons (see above).
+        self.assertFalse(is_rotation("","A"))
+        self.assertFalse(is_rotation("A","AAA"))
+        self.assertFalse(is_rotation("A","B"))
+        self.assertFalse(is_rotation("Hari Seldon","Hari Hari H"))
+        self.assertFalse(is_rotation("abcdefg","defgABC")) # Case-sensitive
+        self.assertFalse(is_rotation("abc defg","defg abc"))
+        self.assertFalse(is_rotation("abc defg","defgabc")) # No space in the second sentence
 
 if __name__ == "__main__":
-
-    sentence = "Hari Seldon"
-    print(is_rotation(sentence, "ldonHari Se")) # True
-    print(is_rotation(sentence, "Hari Hari H")) # False
+    unittest.main()
