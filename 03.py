1f9494e420c1f2f3bd79bf52c2a4cbfe2bb7a6fe
pablogsal <pablogsal@gmail.com>
2015-11-27 17:23:26 +0100

    More love. Reformatted the comments in the unit tests.

--- 03.py
+++ 03.py
@@ -23,12 +23,7 @@ class RotationTests(unittest.TestCase):
         self.assertTrue(is_rotation("abcdefg", "cdefgab"))
         self.assertTrue(is_rotation("ABCdefg", "CdefgAB"))
         self.assertTrue(is_rotation("A", "A"))
-        # The following one is a little tricky. It depends on your definition of "permutation". In the
-        # "active" definition, as there are biyections between {} and itself ( see [1] ) then the
-        # assertion is True. In the "passive" definition it could be false ( see [2] ).
-        # [1] http://math.stackexchange.com/questions/140879/does-there-exist-a-bijection-between-empty-sets
-        # [2] Operations on the Empty set for discussion: https://en.wikipedia.org/wiki/Empty_set
-        self.assertTrue(is_rotation("", "")) # Because reasons (see above).
+        self.assertTrue(is_rotation("", "")) # Because reasons (see *).
         self.assertFalse(is_rotation("","A"))
         self.assertFalse(is_rotation("A","AAA"))
         self.assertFalse(is_rotation("A","B"))
@@ -36,6 +31,14 @@ class RotationTests(unittest.TestCase):
         self.assertFalse(is_rotation("abcdefg","defgABC")) # Case-sensitive
         self.assertFalse(is_rotation("abc defg","defg abc"))
         self.assertFalse(is_rotation("abc defg","defgabc")) # No space in the second sentence
+
+
+	#* This one is a little tricky. It depends on your definition of "permutation". In the
+        # "active" definition, as there are biyections between {} and itself ( see [1] ) then the
+        # assertion is True. In the "passive" definition it could be false ( see [2] ).
+        # [1] http://math.stackexchange.com/questions/140879/does-there-exist-a-bijection-between-empty-sets
+        # [2] Operations on the Empty set for discussion: https://en.wikipedia.org/wiki/Empty_set
+
 
 if __name__ == "__main__":
     unittest.main()
